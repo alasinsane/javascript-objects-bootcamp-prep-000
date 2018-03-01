88795434c7e98447f1e542a22d845c3f44a7b24a
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-03-01 20:44:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
- Object.assign({}, playlist, { [artistName]: songTitle});
  delete playlist.artistName;
  return playlist;
 }
