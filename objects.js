7d271885932eb8f0cb14269926a254035f57031b
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-03-01 20:52:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {
   artist: "songTitle"
 };
+const artistName = "artist"
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle});
